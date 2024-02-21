305679646b84fea2873e3a45c8b4159ef6091d83
ernesto <ernestofamorim@gmail.com>
2024-02-21 14:03:35 -0300

    forms validation

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
-const inputs = document.querySelectorAll("input");
 const password = document.querySelector("#password");
+const passwordMsg = document.querySelector("#password-msg");
 const passwordConfirmation = document.querySelector("#con-password");
 
-function comparePasswords(){
-    const confirmation = password.value === passwordConfirmation.value? false: true;
+function comparePasswords() {
+    const confirmation = password.value === passwordConfirmation.value;
 
-    if(!confirmation){
-        console.log(confirmation);
-    }else{
-        console.log(confirmation);
-    }
+    [password, passwordConfirmation].forEach(element => {
+        element.classList.toggle("error", !confirmation);
+    });
+
+    passwordMsg.innerHTML = confirmation ? "" : "* Passwords do not match!";
 }
 
 passwordConfirmation.addEventListener("blur", () => {
